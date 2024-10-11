af2e7ce9e935297e6ccdc0bcfef062ffcec58252
Jayden Robbins <Jaydenlyricr@gmail.com>
2024-10-11 10:29:09 -0400

    Update 2024_10_11_141814_add_user_agent_to_sessions_table.php

--- database/migrations/2024_10_11_141814_add_user_agent_to_sessions_table.php
+++ database/migrations/2024_10_11_141814_add_user_agent_to_sessions_table.php
@@ -14,7 +14,7 @@ class AddUserAgentToSessionsTable extends Migration
     public function up()
     {
         Schema::table('sessions', function (Blueprint $table) {
-            $table->string('user_agent')->nullable();
+            $table->string('user_agent', 255)->nullable()->after('ip_address');
         });
     }
 
